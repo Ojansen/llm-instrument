a46fe29c34de6bed2cdca5f0b55d8d2633ba95a7
Obe Jansen <obe.jansen@bttr.nl>
2025-07-07 12:25:04 +0200

    feat: Minimal nuxt example

---
+++ nuxt/app.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <button @click="hello">Hello</button>
+  </div>
+</template>
+
+<script setup lang="ts">
+
+
+async function hello() {
+  const {data} = await useFetch('/hello?name=test', {
+    baseURL: 'http://localhost',
+  });
+}
+</script>
