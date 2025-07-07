dd65ad8d2027f32aa09a64bdebb71ca04721b1bb
Obe Jansen <obe.jansen@bttr.nl>
2025-07-07 12:24:31 +0200

    feat: Mount fastapi to docker container

--- api/app/main.py
+++ api/app/main.py
@@ -1,8 +1,22 @@
 import logfire
 import os
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+origins = [
+    "http://localhost",
+    "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 os.environ['OTEL_EXPORTER_OTLP_ENDPOINT'] = 'http://otel-tui:4318'
 logfire.configure(send_to_logfire=False)
